d1b05d7a43b015e96ae26e413a509ba496a79fbd
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-15 19:58:28 +0300

    Make sure the code 101% runs client-side

--- wandering-trader-notifications/src/main/java/io/github/offsetmonkey538/wandering_notifications/mixin/LivingEntityMixin.java
+++ wandering-trader-notifications/src/main/java/io/github/offsetmonkey538/wandering_notifications/mixin/LivingEntityMixin.java
@@ -1,6 +1,10 @@
 package io.github.offsetmonkey538.wandering_notifications.mixin;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.WanderingTraderEntity;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
 
-
+    @Environment(value = EnvType.CLIENT)
     @Inject(method = "onSpawnPacket", at = @At(value = "TAIL"))
     public void onSpawnPacket(EntitySpawnS2CPacket packet, CallbackInfo ci) {
 
