3d2efbb27cf22060dd356346b36bf13082d63926
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-15 20:20:47 +0300

    Send the player a message when the entity is a Wandering Trader

--- wandering-trader-notifications/src/main/java/io/github/offsetmonkey538/wandering_notifications/mixin/LivingEntityMixin.java
+++ wandering-trader-notifications/src/main/java/io/github/offsetmonkey538/wandering_notifications/mixin/LivingEntityMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.WanderingTraderEntity;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,8 +16,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class LivingEntityMixin {
 
     @Environment(value = EnvType.CLIENT)
+    @SuppressWarnings("ConstantConditions")
     @Inject(method = "onSpawnPacket", at = @At(value = "TAIL"))
     public void onSpawnPacket(EntitySpawnS2CPacket packet, CallbackInfo ci) {
-
+        if ((Object) this instanceof WanderingTraderEntity) {
+            MinecraftClient.getInstance().player.sendMessage(Text.of("A wandering trader has spawned at [X: " + (int) packet.getX() + ", Y: " + (int) packet.getY() + ", Z:" + (int) packet.getZ() + "]"));
+        }
     }
 }
