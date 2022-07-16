635044ce297b004aa28b8e03570216c33edfcaf8
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-16 11:52:20 +0300

    Tell the player the trades of the wandering trader

--- wandering-trader-notifications/src/main/java/io/github/offsetmonkey538/wandering_notifications/mixin/LivingEntityMixin.java
+++ wandering-trader-notifications/src/main/java/io/github/offsetmonkey538/wandering_notifications/mixin/LivingEntityMixin.java
@@ -3,10 +3,14 @@ package io.github.offsetmonkey538.wandering_notifications.mixin;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.WanderingTraderEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
 import net.minecraft.text.Text;
+import net.minecraft.village.TradeOffer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,7 +24,26 @@ public class LivingEntityMixin {
     @Inject(method = "onSpawnPacket", at = @At(value = "TAIL"))
     public void onSpawnPacket(EntitySpawnS2CPacket packet, CallbackInfo ci) {
         if ((Object) this instanceof WanderingTraderEntity) {
-            MinecraftClient.getInstance().player.sendMessage(Text.of("A wandering trader has spawned at [X: " + (int) packet.getX() + ", Y: " + (int) packet.getY() + ", Z:" + (int) packet.getZ() + "]"));
+            WanderingTraderEntity wanderingTrader = (WanderingTraderEntity) (Object) this;
+            ClientPlayerEntity player = MinecraftClient.getInstance().player;
+
+            player.sendMessage(Text.of("A wandering trader has spawned at [" + (int) packet.getX() + " / " + (int) packet.getY() + " / " + (int) packet.getZ() + "]"));
+            player.sendMessage(Text.of("Here are the trades:"));
+            wanderingTrader.getOffers().forEach(offer -> {
+                ItemStack firstBuyItem = offer.getAdjustedFirstBuyItem().copy();
+                ItemStack secondBuyItem = offer.getSecondBuyItem().copy();
+                ItemStack sellItem = offer.copySellItem();
+
+                String firstItemText = firstBuyItem.getCount() + " of " + I18n.translate(firstBuyItem.getName().getString());
+                String secondItemText = " and " + secondBuyItem.getCount() + " of " + I18n.translate(secondBuyItem.getName().getString());
+                String sellItemText = sellItem.getCount() + " of " + I18n.translate(sellItem.getName().getString());
+
+                if (secondBuyItem.isEmpty()) secondItemText = "";
+
+                Text message = Text.of(firstItemText + secondItemText + " for " + sellItemText);
+
+                player.sendMessage(message);
+            });
         }
     }
 }
