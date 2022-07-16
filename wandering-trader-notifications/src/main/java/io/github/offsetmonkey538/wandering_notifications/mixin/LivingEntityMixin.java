c1cf22001b894a7654e65073b52ff3305ff42147
OffsetMonkey539 <davgaminggamer@gmail.com>
2022-07-16 14:51:25 +0300

    Add glowing effect to the wandering trader when they spawn

--- wandering-trader-notifications/src/main/java/io/github/offsetmonkey538/wandering_notifications/mixin/LivingEntityMixin.java
+++ wandering-trader-notifications/src/main/java/io/github/offsetmonkey538/wandering_notifications/mixin/LivingEntityMixin.java
@@ -6,25 +6,38 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.passive.WanderingTraderEntity;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
 import net.minecraft.text.Text;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Environment(value = EnvType.CLIENT)
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
 
-    @Environment(value = EnvType.CLIENT)
     @SuppressWarnings("ConstantConditions")
     @Inject(method = "onSpawnPacket", at = @At(value = "TAIL"))
     public void onSpawnPacket(EntitySpawnS2CPacket packet, CallbackInfo ci) {
         if ((Object) this instanceof WanderingTraderEntity) {
+            WanderingTraderEntity wanderingTrader = (WanderingTraderEntity) (Object) this;
+
+            wanderingTrader.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 200));
+
             ClientPlayerEntity player = MinecraftClient.getInstance().player;
 
             player.sendMessage(Text.of(I18n.translate("commands.summon.success", I18n.translate("entity.minecraft.wandering_trader")) + " [" + (int) packet.getX() + " / " + (int) packet.getY() + " / " + (int) packet.getZ() + "]"));
         }
+    }
+
+    @Redirect(method = "isGlowing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;isClient()Z"))
+    public boolean isGlowing(World instance) {
+        return false;
     }
 }
